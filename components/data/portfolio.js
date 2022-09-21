d58318ae42a803d93a914821ae643dd27cd0a92d
Nadim <nahlenadim@gmail.com>
2022-09-21 17:26:06 +0300

    Nextjs Create Data

---
+++ components/data/portfolio.js
@@ -0,0 +1,65 @@
+const portfolio = [
+
+
+  {
+    id: "02",
+    title: "Shopify Ecommerce",
+    img: "/images/shop.PNG",
+    category: "Web App",
+    keyword: ["Next.js", "Shopify", "GraphQL", "Tailwind"],
+    liveUrl: "https://nextjs-shopify-ecommerce-iqfg1tsru-nadim-nahle.vercel.app/",
+    github: 'https://github.com/Nadim-Nahle/Nextjs-Shopify-Ecommerce',
+  },
+
+  {
+    id: "03",
+    title: "WorldCup Nfts",
+    img: "https://media.giphy.com/media/Pk4GtmzYC8gz0pxTXl/giphy.gif",
+    category: "Web App",
+    keyword: ["React", "Gsap", "Styled-Components"],
+    liveUrl: "https://nft-s-website.vercel.app/",
+    github: 'https://github.com/Nadim-Nahle/NFTs-Website',
+  },
+  {
+    id: "01",
+    title: "Movie Fanatics",
+    img: "/images/movie.png",
+    category: "Web App",
+    keyword: ["React", "Node", "Socket", "MongoDB"],
+    liveUrl: "https://github.com/Nadim-Nahle/Movies-Fanatics#implementation",
+    github: 'https://github.com/Nadim-Nahle/Movies-Fanatics',
+  },
+  {
+    id: "04",
+    title: "LeVehicle",
+    img: "/images/django.PNG",
+    category: "Web App",
+    keyword: ["Vue", "Django", "SQLite"],
+    liveUrl: "https://github.com/Nadim-Nahle/Ecommerce-Vue-Django",
+    github: 'https://github.com/Nadim-Nahle/Ecommerce-Vue-Django#implementation',
+  },
+
+  {
+    id: "05",
+    title: "Contacts App",
+    img: "/images/phone.png",
+    category: "Web App",
+    keyword: ["React", "Express", "MongoDB"],
+    liveUrl: "https://github.com/Nadim-Nahle/contacts-app-full-stack",
+    github: 'https://github.com/Nadim-Nahle/contacts-app-full-stack',
+  },
+
+
+  {
+    id: "07",
+    title: "Portfolio Website",
+    img: "/images/portfolio.PNG",
+    category: "Web App",
+    keyword: ["Next.js", "Web design", "UI-UX"],
+    liveUrl: "https://nadim-portfolio.vercel.app",
+    github: 'https://github.com/Nadim-Nahle/portfolio',
+  },
+
+];
+
+export default portfolio;
