c6145eef6258bbc1d93cf55d153a861bb2b601db
Nadim <nahlenadim@gmail.com>
2022-09-21 17:23:17 +0300

    Nextjs Initialize

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import Hero from "../components/UI/Hero";
+import Services from "../components/UI/Services";
+import About from "../components/UI/About";
+import Portfolio from "../components/UI/Portfolio";
+import Contact from "../components/UI/Contact";
+
+export default function Home() {
+  return (
+    <Fragment>
+      <Hero />
+      <About />
+      <Portfolio />
+      <Contact />
+    </Fragment>
+  );
+}
