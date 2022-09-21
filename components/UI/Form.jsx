031c5443fac845a9657d1ff07928f8ca3bd8dc8a
Nadim <nahlenadim@gmail.com>
2022-09-21 17:51:15 +0300

    Nextjs Import useState

--- components/UI/Form.jsx
+++ components/UI/Form.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "../../styles/form.module.css";
 import axios from "axios";
 
 const Form = () => {
   const submitHandler = (e) => {
     e.preventDefault();
+    axios.defaults.headers.post["Content-Type"] = "application/json";
+    axios
+      .post("https://formsubmit.co/ajax/nahlenadim@gmail.com", {
+        name: "FormSubmit",
+        message: "I'm from Devro LABS",
+      })
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
   };
   return (
     <form className={`${classes.form}`} onSubmit={submitHandler}>
