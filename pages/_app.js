33177e846fce4802851a1fd33358ec3c2240eb24
Nadim <nahlenadim@gmail.com>
2023-01-01 13:53:58 +0200

    Portfio Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,26 @@ import "remixicon/fonts/remixicon.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Layout from "../components/Layout/Layout";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-60R7YS3DYP"
+      />
+      <Script strategy="lazyOnload">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-60R7YS3DYP');`}
+      </Script>
+      <Layout>
+        <Component {...pageProps} />;
+      </Layout>
+    </>
   );
 }
 
